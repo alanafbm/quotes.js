1d5714a99d74361e73203407824cf1986d644f38
Alana Moraes <alanaaaa@hotmail.com>
2022-08-16 12:13:33 -0400

    working with regExp and style

--- assets/scripts/Quote.js
+++ assets/scripts/Quote.js
@@ -6,40 +6,61 @@ export default class Quote {
         this.elTemplate = document.querySelector('[data-js-quote-template]')
         this.elQuote = document.querySelector('[data-js-quote]')
         this.elBtn = document.querySelector('button')
-        this.elBlockQuote = this.elTemplate.querySelector('data-js-blockquote')
         this.init();
     }
 
     init(){
         this.clickButton();
+        console.log('init');
+        console.log(this.elBlockQuote);
     }
 
     clickButton(){
         this.elBtn.addEventListener('click', function(){
-            // console.log(this.random().author);
-            //this.random().source;
-            this.injectQuote();
+        let elBlockQuote = document.querySelector('[data-js-blockquote]');
+            if(!elBlockQuote){
+                this.injectQuote();
+            }else{
+                elBlockQuote.remove();
+                this.injectQuote();
+            }
         }.bind(this));
     }
 
-    injectQuote(){
+    injectQuote(){     
+
         const cloneTemplate = this.elTemplate.cloneNode(true);
-
-        cloneTemplate.innerHTML = cloneTemplate.innerHTML.replace('{{quote}}', this.random().quote);
-
-        cloneTemplate.innerHTML = cloneTemplate.innerHTML.replace('{{source}}', this.random().source);
-
-        cloneTemplate.innerHTML = cloneTemplate.innerHTML.replace('{{author}}', this.random().author);
                 
-        cloneTemplate.innerHTML = cloneTemplate.innerHTML.replace('{{nb}}', this.random().);
+        cloneTemplate.innerHTML = cloneTemplate.innerHTML.replace('{{nb}}', this.random().nb);
+
+        const keys = Object.keys(this.random().quotesRandom);
+
+        for (let i = 0; i < keys.length; i++) {
+        
+            let quotesRandom = this.random().quotesRandom;
+            
+            for (const prop in quotesRandom){
+
+                let regExp = new RegExp('{{' + prop + '}}', 'g');
+                cloneTemplate.innerHTML = cloneTemplate.innerHTML.replace(regExp, quotesRandom[prop])
+            }
+                   
+        }
 
 
         const newTemplate = document.importNode(cloneTemplate.content, true);
 
-        let blockQuote = this.elTemplate.firstElementChild;
-
-        console.log(newTemplate);
         this.elQuote.append(newTemplate);
+
+        let elBlockQuote = document.querySelector('[data-js-blockquote]');
+
+        setTimeout(function() {
+            if (elBlockQuote.classList.contains('hidden')) {
+                elBlockQuote.classList.remove('hidden')
+            }
+        }, 100);
+
+
     }
 
     random(){
@@ -47,7 +68,14 @@ export default class Quote {
         const nbQuotes = quotes.length;
 
         const random = Math.floor(Math.random() * (nbQuotes - 1));
+        
+        let nb = random + 1;
+        let quotesRandom = quotes[random];
+        let newQuotes = {
+            quotesRandom,
+            nb
+        };
 
-        return quotes[random];
+        return newQuotes;
     } 
 };
